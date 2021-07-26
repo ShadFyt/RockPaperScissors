a852cb3500062a0a00f73f6e959bd1a226bb2d3f
Ryan Doan <doanryan10@gmail.com>
2021-07-25 21:29:43 -0500

    opponent logic added

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const scoreboard = document.querySelector("#scoreboard");
+const numRounds = document.querySelector("#numRounds");
+let rounds = numRounds.value;
 
 const player = {
   name: "Ryan",
-  score: parseInt(scoreboard.children[0].innerText),
+  score: scoreboard.children[0],
   options: document.querySelector("#options"),
+};
+
+const opponent = {
+  name: "Opponent",
+  score: scoreboard.children[1],
+  options: ["rock", "paper", "scissor"],
+
+  choice: function () {
+    let randNum = Math.floor(Math.random() * 3);
+    return this.options[randNum];
+  },
 };
 
 player.options.addEventListener("click", function (e) {
   if (e.target.id != "options") {
+    player.score.innerText++;
     console.log(e.target.id);
+    console.log(opponent.choice());
   }
+});
+
+numRounds.addEventListener("change", function (e) {
+  console.log(e);
+  rounds = e.target.value;
 });
