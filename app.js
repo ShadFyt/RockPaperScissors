f4dbd200cae8290bc2d0dc2b9805ed0c867d5238
Ryan Doan <doanryan10@gmail.com>
2021-07-26 20:52:28 -0500

    added function to check for a winner

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const scoreboard = document.querySelector("#scoreboard");
 const numRounds = document.querySelector("#numRounds");
 let rounds = numRounds.value;
 
+const selections = {
+  rock: {
+    paper: false,
+    scissor: true,
+    rock: "tie",
+  },
+
+  paper: {
+    rock: true,
+    scissor: false,
+    paper: "tie",
+  },
+
+  scissor: {
+    rock: false,
+    paper: true,
+    scissor: "tie",
+  },
+
+  compare: function (playerOption, opponentOption) {
+    let matchUp = this[playerOption][opponentOption];
+    if (matchUp === true) player.score.innerText++;
+    else if (!matchUp) opponent.score.innerText++;
+    console.log(playerOption);
+    console.log(opponentOption);
+  },
+};
+
 const player = {
   name: "Ryan",
   score: scoreboard.children[0],
@@ -12,18 +40,17 @@ const opponent = {
   name: "Opponent",
   score: scoreboard.children[1],
   options: ["rock", "paper", "scissor"],
-
+  option: "",
   choice: function () {
     let randNum = Math.floor(Math.random() * 3);
-    return this.options[randNum];
+    this.option = this.options[randNum];
   },
 };
 
 player.options.addEventListener("click", function (e) {
   if (e.target.id != "options") {
-    player.score.innerText++;
-    console.log(e.target.id);
-    console.log(opponent.choice());
+    opponent.choice();
+    selections.compare(e.target.id, opponent.option);
   }
 });
 
